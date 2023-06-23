067733b1d5c9789d82c46d8036e2b0ad3eabfeb5
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-23 11:41:21 +0300

    Get Method Done

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 console.log("Hello");
 
 fetch("https://jsonplaceholder.typicode.com/users")
+// get method.
     .then(function (res) {
         return res.json();
     }).then(function (data) {
         console.log(data);
     })
-
 // JSON is a format that allows you to see data from an API.
+
+// Post Method.
